1783e241789d788f3034d9e9c1c96911ddddff0c
djripster <dushyanthan@djsmobiles.com>
2018-10-22 11:51:01 -0400

    Update djsmobiles.js

--- djsmobiles.js
+++ djsmobiles.js
@@ -48,7 +48,30 @@ apstag.fetchBids({
 }); 
 </script>
 
+<!-- Quantcast Tag -->
+<script type="text/javascript">
+var _qevents = _qevents || [];
 
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "https://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+
+_qevents.push({
+qacct:"p-gDf1-KQpvbd8z"
+});
+</script>
+
+<noscript>
+<div style="display:none;">
+<img src="//pixel.quantserve.com/pixel/p-gDf1-KQpvbd8z.gif" border="0" height="1" width="1" alt="Quantcast"/>
+</div>
+</noscript>
+<!-- End Quantcast tag -->
 
 /*
 
