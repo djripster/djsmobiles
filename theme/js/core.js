740e9f46c42329dbf8a480765b99284df2d580c1
djripster <djripster@outlook.com>
2026-07-02 17:59:51 -0400

    Update core.js

--- theme/js/core.js
+++ theme/js/core.js
@@ -1,10 +1,13 @@
 /* DJs Mobiles Theme Core v17
-   Step 1: external file load test only.
-   No migrated logic yet.
+   Step 2: qs helper migrated.
 */
 
 (function () {
   window.DJS = window.DJS || {};
   window.DJS.version = '17.0.0';
   window.DJS.coreLoaded = true;
+
+  window.DJS.qs = function (selector, scope) {
+    return (scope || document).querySelector(selector);
+  };
 })();
